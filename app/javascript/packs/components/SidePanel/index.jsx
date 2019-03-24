650733f85b46b4ea686a534ded540bdd8c117f2c
Wallas Faria <wallasfaria@hotmail.com>
2019-03-24 13:31:04 -0300

    creates SidePanel component

---
+++ app/javascript/packs/components/SidePanel/index.jsx
@@ -0,0 +1,34 @@
+import React, { Component, Fragment } from 'react'
+import PropTypes from 'prop-types'
+
+import Button from '../Button'
+
+require('./style.scss')
+
+class SidePanel extends Component {
+  render() {
+    const { title, children, visible, onClose } = this.props
+
+    return (
+      <Fragment>
+        <div className={`overlay ${visible ? 'is-visible' : ''}`}></div>
+        <div className={`side-panel ${visible ? 'is-visible' : ''}`}>
+          <header>
+            <h2 className='title'>{title}</h2>
+            <Button type='light' iconName='close' onClick={() => onClose()}>fechar</Button>
+          </header>
+
+          <div className="body">
+            {children}
+          </div>
+        </div>
+      </Fragment>
+    )
+  }
+}
+
+SidePanel.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+
+export default SidePanel
